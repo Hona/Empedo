bddd2b487d3c0a9ea841b881fe02b959b27c5c95
Luke Parker <lukemparker@outlook.com>
2020-07-10 16:04:04 +1000

    Update names and path in runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -1,11 +1,11 @@
-echo '-= Stopping template-bot-production Container =-'
-docker container stop template-bot-production
+echo '-= Stopping empedo-production Container =-'
+docker container stop empedo-production
 
 echo '-= Removing Old template-bot-production Container =-'
-docker container rm template-bot-production
+docker container rm empedo-production
 
 echo '-= Building Docker Image from Dockerfile ='
-docker build -t template-bot -f ./src/DiscordBotTemplate/Dockerfile .
+docker build -t empedo -f ./src/Empedo/Dockerfile .
 
 echo '-= Runnning the Image ='
-docker run -e "ENVIRONMENT=DEVELOPMENT" -v $PWD/PRODUCTION:/app/PRODUCTION -v $PWD/DEVELOPMENT:/app/DEVELOPMENT --network host --restart on-failure:5 --name "template-bot-production" -d template-bot
+docker run -e "ENVIRONMENT=DEVELOPMENT" -v $PWD/PRODUCTION:/app/PRODUCTION -v $PWD/DEVELOPMENT:/app/DEVELOPMENT --network host --restart on-failure:5 --name "empedo-production" -d empedo
