edbd44530d8f0815ca8a5064a50f3be698c93b8d
Luke Parker <lukemparker@outlook.com>
2020-07-10 16:00:59 +1000

    Add demo and soldier emoji to config

--- src/Empedo/Models/Config.cs
+++ src/Empedo/Models/Config.cs
@@ -1,9 +1,15 @@
-﻿using Newtonsoft.Json;
+﻿using Discord;
+using Newtonsoft.Json;
 
 namespace Empedo.Models
 {
     public class Config
     {
         [JsonProperty("command_prefix")] public string CommandPrefix { get; set; }
+        [JsonProperty("demoman_emoji")] public string DemomanEmojiString { get; set; }
+        [JsonProperty("soldier_emoji")] public string SoldierEmojiString { get; set; }
+        [JsonIgnore] public Emoji DemomanEmoji => new Emoji(DemomanEmojiString);
+        [JsonIgnore] public Emoji SoldierEmoji => new Emoji(SoldierEmojiString);
+
     }
 }
