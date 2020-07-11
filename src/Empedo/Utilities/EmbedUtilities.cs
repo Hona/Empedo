14a43b523e48ad8a3a2b2626958c18f36a41ffb5
Luke Parker <lukemparker@outlook.com>
2020-07-11 22:30:33 +1000

    Create utility class to create warning emebds

---
+++ src/Empedo/Utilities/EmbedUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+
+namespace Empedo.Utilities
+{
+    public static class EmbedUtilities
+    {
+        public static EmbedBuilder NewWarningEmbed(string text)
+        {
+            return new EmbedBuilder
+            {
+                Description = text
+            }.WithCurrentTimestamp();
+        }
+    }
+}
