9252cbe91a31b449f0a23a42aa7604f5b87f3ec1
Luke Parker <lukemparker@outlook.com>
2020-07-11 22:30:44 +1000

    Reformat and paginate stalktop

--- src/Empedo/Discord/Commands/TempusHubCommands.cs
+++ src/Empedo/Discord/Commands/TempusHubCommands.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -6,27 +7,45 @@ using Discord;
 using Discord.Commands;
 using Empedo.Models;
 using Empedo.Services;
+using Empedo.Utilities;
 using Environment = System.Environment;
 
 namespace Empedo.Discord.Commands
 {
     public class TempusHubCommands : ModuleBase
     {
+        private int _resultsPerPage = 7;
         public Config Config { get; set; }
 
         [Command("stalktop")]
-        public async Task TopPlayersOnlineAsync()
+        public async Task TopPlayersOnlineAsync(int page = 1)
         {
+            if (page < 1)
+            {
+                await ReplyAsync(embed: EmbedUtilities.NewWarningEmbed("Page number must be 1 or greater").Build());
+                return;
+            }
+
             var topPlayersOnline = await TempusHubDataService.GetTopPlayersOnlineAsync();
 
-            var embedBuilder = new EmbedBuilder
-            {
-                Title = "Top Players Online",
-                Description = string.Join(Environment.NewLine, topPlayersOnline.Take(7).Select(x =>
-                    $"{(x.RankClass == 4 ? Config.DemomanEmoji : Config.SoldierEmoji)} Rank {x.Rank} | {x.RealName ?? x.SteamName} | {x.ServerInfo.CurrentMap} | {Format.Url(x.ServerInfo.Alias, $"https://tempushub.xyz/server/" + x.ServerInfo.Id)}"))
-            }.WithCurrentTimestamp();
+            var desiredPlayers = topPlayersOnline.Skip(_resultsPerPage * (page - 1)).Take(_resultsPerPage).ToList();
+            var pageCount = Math.Ceiling((double) topPlayersOnline.Count / _resultsPerPage);
 
-            await ReplyAsync(embed: embedBuilder.Build());
+            if (desiredPlayers.Any())
+            {
+                var embedBuilder = new EmbedBuilder
+                {
+                    Title = "Top Players Online",
+                    Description = string.Join(Environment.NewLine, desiredPlayers.Select(x =>
+                        $"{(x.RankClass == 4 ? Config.DemomanEmoji : Config.SoldierEmoji)} Rank {x.Rank} {Format.Url(Format.Bold(Format.Sanitize(x.RealName ?? x.SteamName)), TempusHubHelper.PlayerUrl(x.TempusId))} on {Format.Url(Format.Bold(Format.Sanitize(x.ServerInfo.CurrentMap)), TempusHubHelper.MapUrl(x.ServerInfo.CurrentMap))} • {Format.Url(x.ServerInfo.Alias, TempusHubHelper.ServerUrl(x.ServerInfo.Id))}")),
+                }.WithFooter($"Page {page} of {pageCount}").WithCurrentTimestamp();
+
+                await ReplyAsync(embed: embedBuilder.Build());
+            }
+            else
+            {
+                await ReplyAsync(embed: EmbedUtilities.NewWarningEmbed("No more results.").Build());
+            }
         }
     }
 }
