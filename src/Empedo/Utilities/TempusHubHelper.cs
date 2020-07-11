23c320e6d79801a7a98f9a37e4c13f5a6bb574a3
Luke Parker <lukemparker@outlook.com>
2020-07-11 22:28:45 +1000

    Create utility class for linking to tempushub pages

---
+++ src/Empedo/Utilities/TempusHubHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empedo.Utilities
+{
+    public static class TempusHubHelper
+    {
+        private static string _tempusHubBase = "https://tempushub.xyz";
+
+        public static string PlayerUrl(int id)
+            => _tempusHubBase + "/player/" + id;
+
+        public static string MapUrl(string name)
+            => _tempusHubBase + "/map/" + name;
+
+        public static string ServerUrl(int id)
+            => _tempusHubBase + "/server/" + id;
+    }
+}
